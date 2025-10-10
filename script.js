161bc3d538e57c079e52295d78854411a3ce8e26
mohamedirsath07 <mohdirsath072005@gmail.com>
2025-10-10 14:01:03 +0530

    Learned JS and DOM

---
+++ script.js
@@ -0,0 +1,16 @@
+let object1={age:33};
+let object2=object1;
+object2.age=22;
+console.log(object2.age);
+
+function sum(a,b){
+    console.log(a+b);
+}
+sum(6,9);
+
+
+const sumof=(a,b,c)=>{
+    console.log(a+b+c);
+
+}
+sumof(2,5,9);
