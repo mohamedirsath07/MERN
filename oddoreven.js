d1c1c23e36bf0c41ffaa5642bfbafb29699a6013
mohamedirsath07 <mohdirsath072005@gmail.com>
2025-10-13 09:35:20 +0530

    Practised JS

---
+++ oddoreven.js
@@ -0,0 +1,11 @@
+
+function oddoreven(num){
+    return num%2!=0;
+
+}
+let num=47;
+console.log(oddoreven(num));
+
+let array=[23,44,55,66,77,88,99];
+const multiply=(x)=> x*5;
+console.log(array.map(multiply));
