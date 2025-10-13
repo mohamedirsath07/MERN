95cd7d2197aec50ae838b0839cc73cd1bc9dac89
mohamedirsath07 <mohdirsath072005@gmail.com>
2025-10-13 14:16:17 +0530

    Created Vite

--- oddoreven.js
+++ oddoreven.js
@@ -1,11 +1,49 @@
 
-function oddoreven(num){
-    return num%2!=0;
+// function oddoreven(num){
+//     return num%2!=0;
 
-}
-let num=47;
-console.log(oddoreven(num));
+// }
+// let num=47;
+// console.log(oddoreven(num));
 
-let array=[23,44,55,66,77,88,99];
-const multiply=(x)=> x*5;
-console.log(array.map(multiply));
+// let array=[23,44,55,66,77,88,99];
+// const multiply=(x)=> x*5;
+// console.log(array.map(multiply));
+
+// let array2=[1,2,3,4,5,6];
+// let sum=array2.reduce((product,current) => product*current, 1 );
+// console.log(sum);
+
+// let arr=[6,8,3,9,1,10]
+// const mul=(x)=>x*3;
+// product=arr.map(mul);
+// let evenn=product.filter((x)=>x%2==0);
+// let summ=evenn.reduce((curr,next)=> curr+next,0);
+// console.log(summ);
+
+// let aa=[6,8,9];
+// const [one,two]=aa;
+// console.log(two);
+
+// let bb=[1,2,3,4,5];
+// const [first,...rest]=bb;
+// console.log(rest);
+
+// let n =[1,2,3,4,5];
+// const [o,...t]=n;
+// console.log(t);
+// let m=[...n,6,7,8,9];
+// console.log(m);
+
+// let n1={x:1,y:2};
+// let n2={a:3,b:4};
+// let spread={...n1,...n2};
+// console.log(spread);
+
+// fetch('https://jsonplaceholder.typicode.com/posts/1')
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+
+// let name ='powerhouse';
+// let age =200;
+// console.log(`my name is ${name} and my age is ${age}`);
