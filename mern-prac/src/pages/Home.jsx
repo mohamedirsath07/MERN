d711a5d701b6ccc3bfa5415f941f144d7cdb18d5
mohamedirsath07 <mohdirsath072005@gmail.com>
2026-02-10 16:24:05 +0530

    fix: Add proper navigation links to Home page for hooks

--- mern-prac/src/pages/Home.jsx
+++ mern-prac/src/pages/Home.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
+      <h3>React Hooks Examples</h3>
       <ul>
         <li>
-          <Link to='/State'>
-          </Link>
+          <Link to='/State'>useState Hook</Link>
+        </li>
+        <li>
+          <Link to='/Effect'>useEffect Hook</Link>
         </li>
       </ul>
     </div>
